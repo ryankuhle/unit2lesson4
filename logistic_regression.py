9535692a61c8ab4714a94d054ed9ed6a2876b6b0
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-09 20:12:31 -0600

    Initial commit

---
+++ logistic_regression.py
@@ -0,0 +1,6 @@
+'''
+Create a new file called 'logistic_regression.py'. For this lesson, we're going to need pandas and statsmodels.
+Load the data.
+Add a column to your dataframe indicating whether the interest rate is < 12%. This would be a derived column that you create from the interest rate column. You name it IR_TF. It would contain binary values, i.e.'0' when interest rate < 12% or '1' when interest rate is >= 12%
+Do some spot checks to make sure that it worked.
+'''
