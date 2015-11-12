b60629b850b545a9d4fc40ae118837c595443c44
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-12 17:17:19 -0600

    Fix IR_TF equation

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,7 +4,7 @@ import statsmodels.api as sm
 loansData = pd.read_csv('loansData_clean.csv')
 
 for x in loansData['Interest.Rate']:
-    loansData['IR_TF'] = (loansData['Interest.Rate'] >= 0.12).astype(int)
+    loansData['IR_TF'] = (loansData['Interest.Rate'] <= 0.12).astype(int)
 
 loansData['Intercept'] = float(1.0)
 
@@ -12,10 +12,8 @@ ind_vars = ['Amount.Requested', 'FICO.Score', 'Intercept']
 
 logit = sm.Logit(loansData['IR_TF'], loansData[ind_vars])
 
-#2 - Fit the model
 result = logit.fit()
 
-#3 - Get coefficients from results
 coeff = result.params
 print coeff
 
