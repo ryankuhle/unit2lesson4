de59c1006086de86b1388ee1f54ff92f123eb702
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-12 17:43:46 -0600

    Create approval function

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,6 +2,19 @@ import pandas as pd
 import statsmodels.api as sm
 import math
 
+def logistic_function(coef, FICO, AMT):
+    p = 1 / (1 + math.e ** -(coef['Intercept'] +
+                             coef['FICO.Score'] * FICO +
+                             coef['Amount.Requested'] * AMT))
+    return p
+
+def approval(p):
+    if p >= 0.7:
+        msg = 'approved'
+    else:
+        msg = 'denied'
+    return msg
+
 loansData = pd.read_csv('loansData_clean.csv')
 
 for x in loansData['Interest.Rate']:
@@ -17,12 +30,7 @@ result = logit.fit()
 
 coeff = result.params
 
-def logistic_function(coef, FICO, AMT):
-    p = 1 / (1 + math.e ** -(coef['Intercept'] +
-                             coef['FICO.Score'] * FICO +
-                             coef['Amount.Requested'] * AMT))
-    return p
+p = logistic_function(coeff, 750, 10000) #Find probability based on FICO Score and Loan Request
+msg = approval(p) #Check approval status based on probability
 
-
-p = logistic_function(coeff, 750, 10000)
-print p
+print 'The loan is {} based on a probability of {:.2%}'.format(msg, p)
