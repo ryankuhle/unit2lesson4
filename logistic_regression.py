d06e4801a960df1ec286a3f9ab977a7df43f3d1e
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-11 18:08:11 -0600

    Create list of independent variables

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,6 +6,6 @@ loansData = pd.read_csv('loansData_clean.csv')
 for x in loansData['Interest.Rate']:
     loansData['IR_TF'] = (loansData['Interest.Rate'] >= 0.12).astype(int)
 
-loansData['intercept'] = float(1.0)
+loansData['Intercept'] = float(1.0)
 
-#print loansData['intercept'][0:5]
+ind_vars = [loansData['Interest.Rate'], loansData['Amount.Requested'], loansData['FICO.Score'], loansData['Intercept']]
